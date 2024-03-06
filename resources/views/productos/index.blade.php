d446482418410d8ffffe701a0537112b71443f24
Humberto Domínguez <humbertodelac13@gmail.com>
2024-03-06 10:51:26 -0600

    módulo productos: búsqueda implementada

--- resources/views/productos/index.blade.php
+++ resources/views/productos/index.blade.php
@@ -123,8 +123,10 @@
                                     </td>
                                     <td class="px-6 py-4">
                                         <form action="{{ route('proveedores') }}" method="get">
-                                            <input type="text" id="search" name="search" value="{{ $producto->nombre_proveedor }}" class="hidden"/>
-                                            <button type="submit" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">{{ $producto->nombre_proveedor }}</button>
+                                            <input type="text" id="search" name="search"
+                                                value="{{ $producto->nombre_proveedor }}" class="hidden" />
+                                            <button type="submit"
+                                                class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">{{ $producto->nombre_proveedor }}</button>
                                         </form>
                                     </td>
                                     <td class="px-6 py-4">
